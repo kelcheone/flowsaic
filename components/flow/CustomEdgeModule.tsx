35b8cbf9c7bd2e3c47b33f6fb4fb16c8e229e458
Kevin Kelche <kelcheone@gmail.com>
2025-02-06 12:57:38 +0300

    feat: custom edges
    - allows deletion of edges.

---
+++ components/flow/CustomEdgeModule.tsx
@@ -0,0 +1,61 @@
+import { CircleX } from "lucide-react";
+import { FC } from "react";
+import {
+  BezierEdge,
+  EdgeLabelRenderer,
+  EdgeProps,
+  getStraightPath,
+  getBezierPath,
+  useReactFlow,
+} from "@xyflow/react";
+import { useModuleFlowStore } from "@/stores/ModulesManager";
+
+const CustomEdgeModule: FC<EdgeProps> = (props) => {
+  const {
+    id,
+    sourceX,
+    sourceY,
+    targetX,
+    targetY,
+    sourcePosition,
+    targetPosition,
+  } = props;
+
+  const { setEdges } = useReactFlow();
+  const flowSetEdges = useModuleFlowStore((state) => state.setEdges);
+  const deleteEdge = useModuleFlowStore((state) => state.deleteEdge);
+
+  const [edgepath, labelX, labelY] = getBezierPath({
+    sourceY,
+    sourceX,
+    targetY,
+    targetX,
+    targetPosition,
+    sourcePosition,
+  });
+
+  return (
+    <>
+      <BezierEdge {...props} />
+      <EdgeLabelRenderer>
+        <button
+          style={{
+            position: "absolute",
+            transform: `translate(-50%, -50%) translate(${labelX}px, ${labelY}px)`,
+            pointerEvents: "all",
+          }}
+          className="nodrag nopan bg-warning rounded-full p-1   cursor-pointer"
+          onClick={() => {
+            setEdges((edges) => edges.filter((e) => e.id !== id));
+            flowSetEdges((edges) => edges.filter((e) => e.id !== id));
+            deleteEdge(id);
+          }}
+        >
+          <CircleX className="w-4 h-4 text-red-300" />
+        </button>
+      </EdgeLabelRenderer>
+    </>
+  );
+};
+
+export default CustomEdgeModule;

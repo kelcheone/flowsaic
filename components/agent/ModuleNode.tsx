4769a731a45c1673affefc4f7e2e01f1e316d77e
Kevin Kelche <kelcheone@gmail.com>
2025-02-07 12:03:21 +0300

    feat: generate or regenerate schema

--- components/agent/ModuleNode.tsx
+++ components/agent/ModuleNode.tsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { useModuleStore } from "@/stores/agentsModuleStore";
-import { Trash2 } from "lucide-react"; // Add this import
+import { FlaskConicalIcon, Trash2 } from "lucide-react"; // Add this import
 import { useFlowStore } from "@/stores/agents/flow";
 import { SchemaDefinition, useNillionStore } from "@/stores/agents/nillion";
 
@@ -39,14 +39,17 @@ const ModuleNode: React.FC<ModuleNodeProps> = ({ id, type, data }) => {
 
   const handleTest = useCallback(() => {
     // runModule(id, type, inputs);
-    console.log(data.schema);
+    console.log(data);
+  }, [id, type, inputs, runModule]);
+
+  const handleIntializeSchema = useCallback(() => {
     (async () => {
       const schemaId = await createSchema(data.schema, data.name);
       updateNodeSchemaId(id, schemaId);
       // also update data.schemaId
       data.schemaId = schemaId;
     })();
-  }, [id, type, inputs, runModule]);
+  }, [data, createSchema, updateNodeSchemaId]);
 
   const handleDelete = useCallback(() => {
     deleteNode(id);
@@ -74,15 +77,25 @@ const ModuleNode: React.FC<ModuleNodeProps> = ({ id, type, data }) => {
             <Label htmlFor={`${id}-${key}`}>{key}</Label>
             <Input
               id={`${id}-${key}`}
-              value={value}
+              placeholder={key}
               onChange={(e) => handleInputChange(key, e.target.value)}
             />
           </div>
         ))}
       </div>
-      <Button onClick={handleTest} className="mt-4">
-        Test Module
-      </Button>
+      <div className="flex justify-between items-center mt-4">
+        <Button onClick={handleIntializeSchema}>
+          {data.schemaId ? "Reinitialize Schema" : "Initialize Schema"}
+        </Button>
+        <Button
+          onClick={handleTest}
+          variant="ghost"
+          size="icon"
+          className="mt-4"
+        >
+          <FlaskConicalIcon className="h-4 w-4 mr-2" />
+        </Button>
+      </div>
       <Handle type="source" position={Position.Bottom} />
     </div>
   );
